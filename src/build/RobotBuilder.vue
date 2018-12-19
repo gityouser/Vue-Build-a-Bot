eebc63e3662b4711e6fc0657c7f38a4ee7bb4302
Andrei Constantinescu <andreiconstantinescu@Andrei-Constantinescus-MBP.local>
2018-12-19 18:39:48 +0200

    added nested routes, html5 history and navigation guards

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -79,10 +79,21 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      // eslint no-alert: 0
+      /* esling no-restricted-globals: 0 */
+      const response = confirm(('You have not added your robot to your carat, are you sure you want to leave?'));
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       parts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -110,6 +121,7 @@ export default {
       + robot.rightArm.cost
       + robot.base.cost;
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
     },
 
   },
---
+++ src/sidebars/SidebarStandard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sidebar">
+    Standard Sidebar
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Standard',
+};
+</script>
+
+<style>
+  .sidebar {
+    font-size: 50px;
+    transform: rotate(90deg);
+  }
+</style>
